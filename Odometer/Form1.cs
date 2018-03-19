f1499c7f6f6e5c80028fdb3ba021ca8d158f02af
Shubham <shubham.rathore@gridinfocom.com>
2018-03-19 16:54:37 +0530

    changes reverted

--- Odometer/Form1.cs
+++ Odometer/Form1.cs
@@ -112,7 +112,7 @@ namespace Odometer
             {
                 //throw new NotImplementedException();
                 Console.OpenStandardOutput('i');
-                tick+=10;
+                tick+=100;
                 int temp, l1, l2, l3;
                 temp = tick;
                 l3 = temp % 10;
